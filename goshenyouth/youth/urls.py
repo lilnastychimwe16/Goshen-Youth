2e0f8a864953d0aaf29dfd31dd22cf89f82ad04a
Takudzwa Chimwe <takudzwachimwe16@gmail.com>
2025-02-24 11:43:12 +0200

    (update)

--- goshenyouth/youth/urls.py
+++ goshenyouth/youth/urls.py
@@ -1,13 +1,10 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
 from django.contrib import admin
 from django.urls import include, path
+from youth import views  # Import views directly if needed
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('youth.urls')),
+    path('admin/', admin.site.urls),  # Admin URL
+    path('', views.index, name='index'),  # Root URL mapped to index view of youth app
+    # If you have more URLs in youth app, include them like below
+    path('youth/', include('youth.urls')),  # Include all youth app URLs
 ]
